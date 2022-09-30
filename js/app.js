4180930bd41249e20dd4244815a189e006e21646
Rongila <rashidrock1234@gmail.com>
2022-10-01 01:00:43 +0600

    first commit

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(".dress").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+});
+$(".slider_mid").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+});
