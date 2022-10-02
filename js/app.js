ed28f67a7a9462cfd8509190d7c2334866996295
Rongila <rashidrock1234@gmail.com>
2022-10-02 20:30:38 +0600

    vivo last

--- js/app.js
+++ js/app.js
@@ -14,3 +14,32 @@ $(".slider_mid").slick({
   slidesToScroll: 1,
   arrows: false,
 });
+$(".item_right_slider_for_hot_deal").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  arrows: false,
+  vertical: true,
+  verticalSwiping: true,
+  asNavFor: ".item_left_slider_for_hot_deal,.item_mid_slider_for_hot_deal",
+});
+$(".item_left_slider_for_hot_deal").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  asNavFor: ".item_mid_slider_for_hot_deal,.item_right_slider_for_hot_deal",
+});
+$(".item_mid_slider_for_hot_deal").slick({
+  dots: false,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  arrows: false,
+  asNavFor: ".item_left_slider_for_hot_deal,.item_right_slider_for_hot_deal",
+});
